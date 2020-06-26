7b7d7582d23914b19369f05422341cbe6126c1ba
florenciaLP <62829148+florenciaLP@users.noreply.github.com>
2020-06-26 04:05:32 -0300

    se agregaron cosas al catálogo y se empezó la parte de compra

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
+// HOME
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, {});
+});
+
+window.addEventListener("load", () => {
+  setTimeout(carga, 1500);
+
+  function carga() {
+    document.getElementById("preloader").className = 'hide';
+    document.getElementById("contenido").className = " ";
+
+
+  }
+});
+
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {
-
-    });
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, {
+    
   });
-
-  window.addEventListener('load', () =>{
-      setTimeout(carga, 1500);
-
-      function carga(){
-    document.getElementById('preloader').className='hide';
-      document.getElementById('contenido').className='';
-      }
-  });
+});
