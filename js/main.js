9a45701befcd42474054c996f5edb987d21ae388
florenciaLP <62829148+florenciaLP@users.noreply.github.com>
2020-07-02 20:41:27 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,20 +5,16 @@ document.addEventListener("DOMContentLoaded", function () {
   var instances = M.Sidenav.init(elems, {});
 });
 
-window.addEventListener("load", () => {
-  setTimeout(carga, 1500);
-
-  function carga() {
-    document.getElementById("preloader").className = 'hide';
-    document.getElementById("contenido").className = " ";
-
-
-  }
-});
-
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.collapsible');
   var instances = M.Collapsible.init(elems, {
     
   });
 });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, {
+      
+    });
+  });
