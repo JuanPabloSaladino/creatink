d8c2765aeaf4784ee10b8f30bb2697d4b835d80a
florenciaLP <62829148+florenciaLP@users.noreply.github.com>
2020-07-07 20:45:18 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,3 +18,82 @@ document.addEventListener('DOMContentLoaded', function() {
       
     });
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, {
+
+    });
+  });
+
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+
+    });
+  });
+
+  //Inicializa TODOS los scripts de JavaScript
+  document.addEventListener('DOMContentLoaded', function () {
+    M.AutoInit();
+});
+
+function cambiarColorBlanco() {
+    document.getElementById("botoncito").style.backgroundColor = "white";
+}
+
+
+function cambiarColorNegro() {
+    document.getElementById("botoncito").style.backgroundColor = "black";
+}
+
+function cambiarColorMarron() {
+    document.getElementById("botoncito").style.backgroundColor = "#795548";
+}
+
+function cambiarColorVioleta() {
+    document.getElementById("botoncito").style.backgroundColor = "#9c27b0";
+}
+
+function cambiarColorMagenta() {
+    document.getElementById("botoncito").style.backgroundColor = "#e91e63";
+}
+
+function cambiarColorAzul() {
+    document.getElementById("botoncito").style.backgroundColor = "#2196f3";
+}
+
+function cambiarColorTurquesa() {
+    document.getElementById("botoncito").style.backgroundColor = "#00bcd4";
+}
+
+function cambiarColorVerde() {
+    document.getElementById("botoncito").style.backgroundColor = "#4caf50";
+}
+
+function cambiarColorDorado() {
+    document.getElementById("botoncito").style.backgroundColor = "#ffc107";
+}
+
+function cambiarColorAmarillo() {
+    document.getElementById("botoncito").style.backgroundColor = "#ffeb3b";
+}
+
+function cambiarColorNaranja() {
+    document.getElementById("botoncito").style.backgroundColor = "#ff9800";
+}
+
+function cambiarColorRojo() {
+    document.getElementById("botoncito").style.backgroundColor = "#f44336";
+}
+
+var instance = M.Tabs.init(el, {
+    
+});
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {
+        
+    });
+  });
