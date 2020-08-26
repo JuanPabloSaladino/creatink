4050d48a8b2ea4edab1b744b293fe1b57295aca0
florenciaLP <62829148+florenciaLP@users.noreply.github.com>
2020-08-26 16:37:36 -0300

    Add files via upload
    espero que sean los cambios finales

--- js/main.js
+++ js/main.js
@@ -5,45 +5,37 @@ document.addEventListener("DOMContentLoaded", function () {
   var instances = M.Sidenav.init(elems, {});
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.collapsible');
   var instances = M.Collapsible.init(elems, {
-    
+
   });
 });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, {
-      
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, {
+
   });
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems, {
-
-    });
-  });
-
-    document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-
-    });
-  });
-
-  //Inicializa TODOS los scripts de JavaScript
-  document.addEventListener('DOMContentLoaded', function () {
-    M.AutoInit();
 });
 
-function cambiarCorazon(){
-  var corazon =document.getElementById("corazon");
-   corazon.innerHTML="favorite";
-   
-  
-}
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems, {
+
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+
+  });
+});
+
+//Inicializa TODOS los scripts de JavaScript
+document.addEventListener('DOMContentLoaded', function () {
+  M.AutoInit();
+});
 
 
-  
